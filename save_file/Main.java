dcd5eb2fbb4c818b1bbc9edfeeb4e5c31ecca5cd
tuanbui1008 <buiminhtuanbuiminhtuan@gmail.com>
2023-11-09 10:13:45 +0700

    update

--- save_file/Main.java
+++ save_file/Main.java
@@ -1,7 +1,14 @@
+ 
+-import org.apache.log4j.BasicConfigurator;
+ import org.eclipse.jgit.api.Git;
+ import org.eclipse.jgit.api.errors.GitAPIException;
+ import org.eclipse.jgit.diff.DiffEntry;
+@@ -17,10 +16,10 @@
+ 
  public class Main {
      public static void main(String[] args) throws IOException, GitAPIException {
 -        System.out.println("Hello world!");
-         BasicConfigurator.configure();
+-        BasicConfigurator.configure();
          // Đường dẫn đến thư mục repository
 -        String repositoryPath = "D:/demo/demo";
         final String dir = System.getProperty("user.dir");
@@ -10,7 +17,7 @@
  
          // Tạo đối tượng Git từ đường dẫn repository
          try (Git git = Git.open(new File(repositoryPath))) {
-@@ -28,7 +29,7 @@
+@@ -28,7 +27,7 @@
              ObjectId masterCommitId = git.getRepository().resolve("master");
  
              // Lấy ra ObjectId của commit trên nhánh khác
@@ -19,7 +26,7 @@
  
              // Lấy ra danh sách các tệp thay đổi giữa hai commit
              List<DiffEntry> diffEntries = git.diff()
-@@ -42,8 +43,8 @@
+@@ -42,8 +41,8 @@
                  }
                  String[] path = diffEntry.getNewPath().split("/");
                  String pathFile = path[path.length - 1];
@@ -30,7 +37,7 @@
                  if (!file.getName().endsWith(".java")) {
                      continue;
                  }
-@@ -74,7 +75,7 @@
+@@ -74,7 +73,7 @@
                  reader.close();
                  writer.close();
              }
